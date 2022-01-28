59347c7227c9e07a4caf8d53c5a05e6dba7b3cbc
Iván Eduardo <eduardo.alba94@hotmail.com>
2022-01-28 13:49:55 -0600

    Axios cargado desde app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((result) => {
+      console.log(result.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-        <header className="App-header">
-
-        </header>
-        <main>
-            
-        </main>
+      <header className="App-header"></header>
+      <main></main>
     </div>
   );
 }
