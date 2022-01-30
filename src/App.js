e7c924fd494741fd8a2760027a70ef5af4fc82e6
Iván Eduardo <eduardo.alba94@hotmail.com>
2022-01-30 17:30:47 -0600

    Seleccion de 5 elementos

--- src/App.js
+++ src/App.js
@@ -3,18 +3,56 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Error from "./components/Error";
 import TodoList from "./components/TodoList";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((result) => {
-      //console.log(result.data);
-      setTodos(result.data);
-    });
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5")
+      .then((result) => {
+        //console.log(result.data);
+        setTodos(result.data);
+      });
   }, []);
 
-  return <div>{todos ? <TodoList todos={todos} /> : <Error />}</div>;
+  const add = () => {
+    setTodos();
+  };
+
+  return (
+    <div className="App">
+      <section className="Add-Todos">
+        <Container>
+          <Form>
+            <Form.Group controlId="form.Name">
+              <Form.Label>Tarea</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ingrese nombre de la tarea"
+              />
+            </Form.Group>
+            <Form.Group controlId="form.Button">
+              <Button className="btn-add" variant="primary">
+                ADD
+              </Button>
+            </Form.Group>
+          </Form>
+        </Container>
+        {add}
+      </section>
+      <hr />
+      <section className="List-All">
+        <div className="h1">Lista Todo´s</div>
+        <div className="List">
+          {todos ? <TodoList todos={todos} /> : <Error />}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
